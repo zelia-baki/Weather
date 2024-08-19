9e632a48e8a222b49379ef25423326c847470435
jalytsim <nomenatsimijaly@gmail.com>
2024-08-19 20:11:58 +0300

    ok

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000', // Remplacez par l'IP et le port de votre serveur
+  baseURL: 'https://164.92.211.54', // Remplacez par l'IP et le port de votre serveur
   headers: {
     'Content-Type': 'application/json',
   },
